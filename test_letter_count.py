42dff1bb4cda499343930a7fb340e3474388256e
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-22 19:05:56 -0600

    [IMP] test_*: make results more descriptive

--- test_letter_count.py
+++ test_letter_count.py
@@ -1,9 +1,59 @@
 import unittest
 from letter_count import letter_count
 
-def test_01_letter_count(self):
-    """Case 1: Hola mundo is the string, the result must be {h: 1, o: 2, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}"""
-    print(letter_count('Hola mundo'))
+class Test(unittest.TestCase):
+    def test_01_letter_count(self):
+        """Case 1: `Hola mundo` is the string, the result must be {h: 1, o: 2, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}"""
+        expected_result = {'h': 1, 'l': 1, 'a': 1, 'm': 1, 'u': 1, 'n': 1, 'd': 1, 'o': 2}
+        result = letter_count('Hola mundo')
+        self.assertEqual(
+            result,
+            expected_result,
+            "Error! The result must be %s but get %s instead" % (expected_result, result)
+        )
+
+    def test_02_letter_count(self):
+        """Case 2: `Hola mundo` is the string, the result must be {h: 1, o: 2, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}
+        but the dict to be compared has less keys"""
+        expected_result = {'h': 1, 'l': 1, 'a': 1, 'm': 1, 'u': 1, 'n': 1, 'd': 1}
+        result = letter_count('Hola mundo')
+        self.assertNotEqual(
+            result,
+            expected_result,
+            "Error! The result must be %s but get %s instead" % (expected_result, result)
+        )
+
+    def test_03_letter_count(self):
+        """Case 3: `Hola mundo` is the string, the result must be {h: 1, o: 2, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}
+        but the dict to be compared has more keys"""
+        expected_result = {'h': 1, 'l': 1, 'a': 1, 'm': 1, 'u': 1, 'n': 1, 'd': 1, 'o': 2, 'j': 2, 'k': 3}
+        result = letter_count('Hola mundo')
+        self.assertNotEqual(
+            result,
+            expected_result,
+            "Error! The result must be %s but get %s instead" % (expected_result, result)
+        )
+
+    def test_04_letter_count(self):
+        """Case 4: `Hola mundo` is the string, the result must be {h: 1, o: 2, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}
+        but the dict to be compared has incorrect numbers of lettes"""
+        expected_result = {'h': 10, 'l': 2, 'a': 1, 'm': 1, 'u': 1, 'n': 11, 'd': 6, 'o': 2}
+        result = letter_count('Hola mundo')
+        self.assertNotEqual(
+            result,
+            expected_result,
+            "Error! The result must be %s but get %s instead" % (expected_result, result)
+        )
+
+    def test_05_letter_count(self):
+        """Case 5: `Holamundooo O` is the string, the result must be {h: 1, o: 5, l: 1, a: 1, m: 1, u: 1, n: 1, d: 1}"""
+        expected_result = {'h': 1, 'l': 1, 'a': 1, 'm': 1, 'u': 1, 'n': 1, 'd': 1, 'o': 5}
+        result = letter_count('Holamundooo O')
+        self.assertEqual(
+            result,
+            expected_result,
+            "Error! The result must be %s but get %s instead" % (expected_result, result)
+        )
 
 if __name__ == '__main__':
     unittest.main()
--- test_palindrome.py
+++ test_palindrome.py
@@ -5,27 +5,53 @@ class Test(unittest.TestCase):
 
     def test_01_is_palindrome(self):
         """Case 1: The string `ddedd` is given to check if it's a palindrome, the result must be True"""
-        string = "ddedd"
-        self.assertEqual(is_palindrome(string), "The string ddedd is a palindrome.")
+        self.assertEqual(
+            is_palindrome("ddedd"),
+            "The string ddedd is a palindrome.",
+            "Error! The result should be a palindrome.")
 
     def test_02_is_palindrome(self):
         """Case 2: The string `ddewddw` is given to check if it's a palindrome, the result must be False"""
-        string = "ddewddw"
-        self.assertEqual(is_palindrome(string), "The string ddewddw isn't a palindrome.")
+        self.assertEqual(
+            is_palindrome("ddewddw"),
+            "The string ddewddw isn't a palindrome.",
+            "Error! The result shouldn't be a palindrome.")
 
     def test_03_is_palindrome(self):
         """Case 3: The string `wddw` is given to check if it's a palindrome, the result must be True"""
-        string = "wddw"
-        self.assertEqual(is_palindrome(string), "The string wddw is a palindrome.")
+        self.assertEqual(
+            is_palindrome("wddw"),
+            "The string wddw is a palindrome.",
+            "Error! The result should be a palindrome.")
 
     def test_04_is_palindrome(self):
         """Case 4: The string `wdwd` is given to check if it's a palindrome, the result must be False"""
-        string = "wdwd"
-        self.assertEqual(is_palindrome(string), "The string wdwd is a palindrome.")
+        self.assertEqual(
+            is_palindrome("wdwd"),
+            "The string wdwd is a palindrome.",
+            "Error! The result shouldn't be a palindrome")
 
     def test_05_is_palindrome(self):
-        """Case 5: The string is empty, the result must be a string indicating to input other value"""
-        self.assertEqual(is_palindrome(""), "Please enter a valid string.")
+        """Case 5: The string is empty, the result should be a string indicating to input other value"""
+        self.assertEqual(
+            is_palindrome(""),
+            "Please enter a valid string.",
+            "Error! The result should be an indication to input other string.")
+
+    def test_06_is_palindrome(self):
+        """Case 6: The string `wDdw` is given to check if it's a palindrome, the result must be True, even with upper
+        case letters"""
+        self.assertEqual(
+            is_palindrome('wdDdw'),
+            "The string wdDdw is a palindrome.",
+            "Error! The result should be a palindrome.")
+
+    def test_07_is_palindrome(self):
+        """Case 7: The string `wdDdw a` is given to check if it's a palindrome, the result must be False"""
+        self.assertEqual(
+            is_palindrome('wdDdw a'),
+            "The string wdDdw a isn't a palindrome.",
+            "Error! The result shouldn't be a palindrome")
 
 
 if __name__ == '__main__':
