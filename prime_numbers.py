cd0ad0d773a44ae2e2a98f514523705b52dbe1e8
Yoany Mora Vauxoo <yoany@vauxoo.com>
2023-11-22 18:07:00 -0600

    [ADD] prime_numbers: validate if a number is, or not, a prime number

---
+++ prime_numbers.py
@@ -0,0 +1,36 @@
+def is_prime_number(number):
+    """Indicate if a number is, or not, a prime number.
+
+    To solve this problem I'm using the Divisibility Tests to find the number of factors of the input,
+    if this is bigger than 2 the input isn't prime.
+
+    Params:
+        number (int): Value to check if it's, or not, a prime number.
+    Returns:
+        string indicating if the input is a prime number"""
+
+    if number <= 0:
+        return "Please input a number bigger than 0"
+
+    is_prime = False
+    if number > 0 and number <= 10:
+        factors = 0
+        for N in range(1, 10):
+            if N <= number and number % N == 0:
+                factors += 1
+
+    if number >= 10:
+        factors = 2
+        if str(number)[-1] != '0' and str(number)[-1] != '5':
+            if number % 2 == 0 or number % 3 == 0 or number % 9 == 0:
+                factors += 1
+        else:
+            factors += 1
+
+    is_prime = factors == 2
+
+    return "The number %s %s prime." % (number, "is" if is_prime else "isn't")
+
+# To run this program with a user input please uncomment those lines
+# number = input("Give me a number: ")
+# print(is_prime_number(number))
---
+++ test_prime_number.py
@@ -0,0 +1,83 @@
+import unittest
+from prime_numbers import is_prime_number
+
+class Test(unittest.TestCase):
+
+    def test_01_is_prime_number(self):
+        """Case 1: 1 has only one factor therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(1), "The number 1 isn't prime.")
+
+    def test_02_is_prime_number(self):
+        """Case 2: 2 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(2), "The number 2 is prime.")
+
+    def test_03_is_prime_number(self):
+        """Case 3: 3 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(3), "The number 3 is prime.")
+
+    def test_04_is_prime_number(self):
+        """Case 4: 4 is divisible by 1, 2 and 4 thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(4), "The number 4 isn't prime.")
+
+    def test_05_is_prime_number(self):
+        """Case 5: 5 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(5), "The number 5 is prime.")
+
+    def test_06_is_prime_number(self):
+        """Case 6: 6 is divisible by 1, 2, 3 and 6 thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(6), "The number 6 isn't prime.")
+
+    def test_07_is_prime_number(self):
+        """Case 7: 7 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(7), "The number 7 is prime.")
+
+    def test_08_is_prime_number(self):
+        """Case 8: 8 is divisible by 1, 2, 4 and 8 thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(8), "The number 8 isn't prime.")
+
+    def test_09_is_prime_number(self):
+        """Case 9: 9 is divisible by 1, 3, and 9 thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(9), "The number 9 isn't prime.")
+
+    def test_10_is_prime_number(self):
+        """Case 10: 17 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(17), "The number 17 is prime.")
+
+    def test_11_is_prime_number(self):
+        """Case 11: 18 last digit is pair thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(18), "The number 18 isn't prime.")
+
+    def test_12_is_prime_number(self):
+        """Case 12: 89 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(89), "The number 89 is prime.")
+
+    def test_13_is_prime_number(self):
+        """Case 13: 95 last digit is 5 thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(95), "The number 95 isn't prime.")
+
+    def test_14_is_prime_number(self):
+        """Case 14: 97 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(97), "The number 97 is prime.")
+
+    def test_15_is_prime_number(self):
+        """Case 15: 673 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(673), "The number 673 is prime.")
+
+    def test_16_is_prime_number(self):
+        """Case 16: 674 last digit is pair thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(674), "The number 674 isn't prime.")
+
+    def test_17_is_prime_number(self):
+        """Case 17: 997 has 2 factors therefore is a prime number."""
+        self.assertEqual(is_prime_number(997), "The number 997 is prime.")
+
+    def test_18_is_prime_number(self):
+        """Case 18: 998 last digit is pair thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(998), "The number 998 isn't prime.")
+
+    def test_19_is_prime_number(self):
+        """Case 19: 678 last digit is pair thus has more than 2 factors therefore isn't a prime number."""
+        self.assertEqual(is_prime_number(678), "The number 678 isn't prime.")
+
+if __name__ == '__main__':
+    unittest.main()
